c8c59ce5403ffce1c208d38b6932c8618fa045d7
Felipe Chagas <felipechagas@google.com>
2025-09-30 11:35:22 -0300

    Change Dependecies

--- angular/src/dependencies/glue/lib/constants/classes.js
+++
@@ -1,9 +0,0 @@
-/**
- * @fileoverview A list of Glue classes.
- */
-var CssClasses;
-(function(CssClasses) {
-CssClasses['SHOW'] = 'glue-is-shown';
-CssClasses['ACTIVE'] = 'glue-is-active';
-})(CssClasses || (CssClasses = {}));
-export {CssClasses};
--- angular/src/dependencies/glue/src/header/deepnav/constants.ts
+++
@@ -1,6 +0,0 @@
-enum Strings {
-  ERROR_MSG_NO_CHILDREN = 'has no element children.',
-  ERROR_PREFIX = 'Menubar constructor argument el ',
-}
-
-export {Strings};
